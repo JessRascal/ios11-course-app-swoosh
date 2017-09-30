3ee7a5b4a2217258abf5abdc39cf7d7cfd866e88
Jess Rascal <jess@justonejess.com>
2017-09-30 18:03:09 +0100

    Lesson 9 complete

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jess Rascal on 30/09/2017.
+//  Copyright © 2017 jessrascal. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
